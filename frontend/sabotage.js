3742bdffd7040a1d0edfd8dad6ec6760392e8a8b
Lauren McCarthy <laurmccarthy@gmail.com>
2013-02-14 16:23:02 -0500

    hooray working syss

---
+++ frontend/sabotage.js
@@ -0,0 +1,96 @@
+
+var socket;
+
+function parseUri (str) {
+	var	o   = parseUri.options,
+		m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+		uri = {},
+		i   = 14;
+
+	while (i--) uri[o.key[i]] = m[i] || "";
+
+	uri[o.q.name] = {};
+	uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+		if ($1) uri[o.q.name][$1] = $2;
+	});
+
+	return uri;
+};
+
+parseUri.options = {
+	strictMode: false,
+	key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+	q:   {
+		name:   "queryKey",
+		parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+	},
+	parser: {
+		strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+		loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+	}
+};
+
+
+
+function connect() {
+	console.log('connected');
+	
+	socket = new eio.Socket({ host: location.hostname, port: 8081 });
+	var args = parseUri(document.URL).queryKey;
+  var d = args.delay ? parseFloat(args.delay) : 0;
+  if (args.docName) {
+		// Send up options.
+		socket.send(JSON.stringify({
+		  event: "loadDoc",
+		
+		  data: {
+		    // Pass up the document name if it's set.
+		    docName: args.docName,
+		
+		    // TODO What is this?
+		    delay: d
+		  }
+		}));
+	}
+  socket.on("message", function(msg) {
+  	message(msg);
+	});
+}
+
+    
+
+    
+function message(data) {
+
+	data = JSON.parse(data);
+	
+	switch(data.type) {
+		case 'live':
+			console.log('live');
+			break;
+		case 'word':
+			handleWord(data);
+			break;
+		case 'sentenceEnd':
+			handleSentenceEnd(data);
+			break;
+		case 'stats':
+			handleStats(data);
+			break;
+		default:
+			break;
+	}
+}
+
+
+function handleWord(msg) {
+	console.log('word');
+}
+
+function handleSentenceEnd(msg) {
+	console.log('sentenceEnd');	
+}
+
+function handleStats(msg) {
+	console.log('stats');
+}
