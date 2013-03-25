3951332a59937c2f6ebb2f27e110fa587485037e
Chen Blume <chen@sosolimited.com>
2013-03-25 14:03:13 -0700

    got scraping working

--- youtube/sabotage.js
+++ youtube/sabotage.js
@@ -79,34 +79,69 @@ function auth(client_id) {
   });
 }
 
+
 function appendResults(text) {
   var results = document.getElementById('results');
   results.appendChild(document.createElement('P'));
   results.appendChild(document.createTextNode(text));
 }
 
-function makeRequest() {
-  var request = gapi.client.urlshortener.url.get({
-    'shortUrl': 'http://youtu.be/NLqAF9hrVbY'
-  });
-  request.execute(function(response) {
-    appendResults(response.longUrl);
-  });
+
+function display(data) {
+ console.log(data);
+} 
+
+
+var parseXml;
+
+if (typeof window.DOMParser != "undefined") {
+    parseXml = function(xmlStr) {
+        return ( new window.DOMParser() ).parseFromString(xmlStr, "text/xml");
+    };
+} else if (typeof window.ActiveXObject != "undefined" &&
+       new window.ActiveXObject("Microsoft.XMLDOM")) {
+    parseXml = function(xmlStr) {
+        var xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async = "false";
+        xmlDoc.loadXML(xmlStr);
+        return xmlDoc;
+    };
+} else {
+    throw new Error("No XML parser found");
 }
+
 
 function load() {
 
 	console.log("load");
 	var config = JSON && JSON.parse(configJSON) || $.parseJSON(configJSON);
-	console.log(config);
-
-	auth(config.youtube.client_id);
-
-  gapi.client.setApiKey(config.youtube.api_key);
-  gapi.client.load('urlshortener', 'v1', makeRequest);
+	//console.log(config);
+	
+	
+	$.get('http://www.youtube.com/watch?v=rDiGYuQicpA', function(data) {
+	  //$('#results').html(data);
+	  //console.log(data);
+	  var startInd = data.indexOf("ttsurl") + 10;
+	  var endInd = data.indexOf('"', startInd);
+	  var url = data.substring(startInd, endInd);
+	  url = url.replace(/\\u0026/g, "&").replace(/\\\//g, "/") + "&type=track&lang=en&name&kind=asr&fmt=1";
+	  
+	  console.log(url);
+	  
+	  $.get(url, function(ccStr) {
+			var captions=ccStr.getElementsByTagName("text");
+			for (var i=0; i<captions.length; i++) {
+				console.log(i+" "+captions[i].getAttribute("start")+" "+captions[i].getAttribute("dur")+" "+captions[i].childNodes[0].nodeValue);
+			}
+	  }, 'xml');
+	  
+	});
+	
+	
+	//makeRequest();
+  //gapi.client.setApiKey(config.youtube.api_key);
+ // gapi.client.load('urlshortener', 'v1', makeRequest);
 }
-
-
 
 
 
