5843e57d660e4aa0dde22e1c46f7af2978bec41c
Justin Manor <justin@sosolimited.com>
2013-05-07 13:34:58 -0700

    using good captions from Obama

--- youtube/sabotage.js
+++ youtube/sabotage.js
@@ -44,7 +44,7 @@ function start() {
 	$('#playButton').show(); 
 	$('#muteButton').show();
 	$("#sourceVid").attr("src", embedUrl+'?enablejsapi=1');
-	ytplayer.cueVideoById("ci5p1OdVLAc");
+	ytplayer.cueVideoById("mox4InKEwgU");
 	
 }
 
