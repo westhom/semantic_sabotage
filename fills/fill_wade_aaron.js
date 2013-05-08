4dfcd45db0d9ab00fedc73aefb4f259d14906ade
wadeaaron <wade@sosolimited.com>
2013-05-08 11:06:52 -0400

    first commit-- fill_wade_aaron.js

---
+++ fills/fill_wade_aaron.js
@@ -0,0 +1,44 @@
+var mode = function() {
+
+	return {
+	
+		name: "Wade Aaron",
+		
+		// Handle incoming word message.
+		handleWord: function(msg) {
+			//console.log('word '+msg.word);
+						
+			this.appendWordInContext(msg);
+		},
+		
+		// Handle incoming sentenceEnd message.
+		handleSentenceEnd: function(msg) {
+			console.log('sentenceEnd');	
+		},
+		
+		// Handle incoming stats message.
+		handleStats: function(msg) {
+			console.log(msg);
+		},
+		
+		appendWordInContext: function(msg) {
+		
+		 	// update curSentence
+		 	//if (!msg.sentenceStartFlag && !msg.punctuationFlag)
+		 	//	$('#words').append(' ');
+		 		
+		 	//$('#words').append(msg.word);
+
+		 	// update curSentence
+		 	if (!msg.sentenceStartFlag && !msg.punctuationFlag)
+		 		$('#words').append(' ');
+		 		
+		 	var s = document.createElement('span');
+		 	s.innerHTML = msg.word;
+
+		 	if ($.inArray('funct', msg.cats) >= 0) $(s).addClass('funct');
+
+		 	$('#words').append(s);
+		}
+	}
+};
