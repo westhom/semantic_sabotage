e1958f3c011461825abe05a2326175133b2c6f6d
wadeaaron <wade@sosolimited.com>
2013-05-13 16:11:48 -0400

    navbar position and z-index tweak

--- fills/wade_aaron.js
+++ fills/wade_aaron.js
@@ -7,14 +7,16 @@ var mode = function(id) {
 		el: $('<div class="modeContainer" id="'+id+'"></div>'),
 		
 		// Anything you want to do to initialize your mode. 
+		// This gets called once after the mode is created.
 		init: function() {
+		},
 
+		// Gets called evertime you go to the mode.
+		enter: function() {
 		},
 
 		// Handle incoming word message.
 		handleWord: function(msg) {
-			//console.log('word '+msg.word);
-						
 			this.appendWordInContext(msg);
 		},
 		
@@ -39,9 +41,15 @@ var mode = function(id) {
 			s.innerHTML = msg.word;
 
 			if (msg.cats.length > 0) { 
-				$(s).addClass('aaron');
-				if ($.inArray('funct', msg.cats) >= 0) $(s).addClass('funct');
-				if ($.inArray('verb', msg.cats) >= 0) $(s).addClass('verb');
+				$(s).addClass('aaron proxima-nova-400');
+				if ($.inArray('funct', msg.cats) >= 0) {
+					$(s).removeClass('proxima-nova-400');
+					$(s).addClass('funct proxima-nova-700');
+				}
+				if ($.inArray('verb', msg.cats) >= 0)  {
+					$(s).removeClass('proxima-nova-400');
+					$(s).addClass('verb proxima-nova-700');
+				}
 			}
 
 			this.el.append(s);
