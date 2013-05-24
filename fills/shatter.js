99036c61164192b98ba544e964f06a1cd50d39cf
Justin Manor <justin@sosolimited.com>
2013-05-24 09:46:39 -0700

    made shatter sort letters

--- fills/shatter.js
+++ fills/shatter.js
@@ -4,18 +4,19 @@ var mode = function(id) {
 	
 		name: "Shatter",
 		defaultURL: "http://www.youtube.com/watch?v=mox4InKEwgU",
-		el: $('<div class="modeContainer" id="'+id+'"></div>'),
+		el: $('<div class="modeContainer" style="background-color:black" id="'+id+'"></div>'),
 
 		tFall: 1,
 		tSpread: 3,
 		tFade: 15, // Greater than this.tSpread
 		tColor: 0.5,
-		radius: (Math.max($(window).width(),$(window).height()))*0.75,
+		radius: (Math.max($(window).width(),$(window).height()))*0.5,
 		viewScalar: 1,
 		widthScalar: 20,
 		fontHeight: 100,
 		fallStart: 500,
-		fallEnd: -2000,
+		fallEnd: 0,
+		letterCounts: [],
 
 		// Anything you want to do to initialize your mode. 
 		// This gets called once after the mode is created.
@@ -24,6 +25,9 @@ var mode = function(id) {
 			$('#shatContainer').css('-webkit-transform','perspective(' + $(window).width() + 'px)');
 			this.el.append('<span id="shatShell"></span>');
 			$('#shatShell').addClass('museo-slab-500');
+
+			for (var i = 0; i < 200 ; i++)
+				this.letterCounts[i] = 0;
 		},
 
 		// Gets called evertime you go to the mode.
@@ -33,7 +37,7 @@ var mode = function(id) {
 
 		// Handle incoming word message.
 		handleWord: function(msg) {
-			console.log('word '+msg.word);
+			//console.log('word '+msg.word);
 			this.appendWordInContext(msg);		
 		},
 
@@ -61,6 +65,7 @@ var mode = function(id) {
 				$(m).addClass('morter museo-slab-500');
 
 				var p = [];
+				var charVal = [];
 				var w = 0;
 				
 				for (var i = 0; i < msg.word.length; i++) {
@@ -80,28 +85,45 @@ var mode = function(id) {
 
 					$(ph).append(p[i]);
 
+					//Letter 'A' == charCode 65
+					charVal[i] = msg.word[i].charCodeAt() - 65;
+					if (charVal[i] < 0)
+						charVal[i] = 0; //don't want negative array positions 
+					if (charVal[i] > 31)
+						charVal[i] = charVal[i] - 32; //make 'a' be same as 'A'
+
+					//increment # of times you've seen the letter
+					this.letterCounts[charVal[i]]++;
+
+					//if letters drip off-screen, reset count to put them back on top
+					if (this.letterCounts[charVal[i]] > 100)
+						this.letterCounts[charVal[i]] = 0;
+					
 					$(m).append(ph);
 
 					w = w + width;
 
 				};
 
-				$(m).css('-webkit-transform','translate3d(' + (-w/2) + 'px,' + (-this.fontHeight*2/3) + 'px,' + this.fallStart + 'px)');
+				$(m).css('-webkit-transform','translate3d(' + (-w/2) + 'px,' + (this.radius-this.fontHeight*2/3) + 'px,' + this.fallStart + 'px)');
 			 	$('#shatContainer').append(m);
 				
 				window.getComputedStyle(m).WebkitTransform;
 
 				$(m).css('-webkit-transition','-webkit-transform ' + this.tFall + 's ease-in, color ' + this.tColor + 's linear');
 				$(m).css('-webkit-transform','translate3d(' + (-w/2) + 'px,' + (-this.fontHeight*2/3) + 'px,' + this.fallEnd + 'px)');
-				$(m).css('color', 'rgb(' + Math.floor(Math.random()*150) + ',' + Math.floor(Math.random()*150) + ',' + Math.floor(Math.random()*150) + ')');
+				//$(m).css('color', 'rgb(' + Math.floor(Math.random()*150) + ',' + Math.floor(Math.random()*150) + ',' + Math.floor(Math.random()*150) + ')');
 
 				for (var i = 0; i < msg.word.length; i++) {
 					$(p[i]).css('-webkit-transition', '-webkit-transform ' + this.tSpread + 's ease-out, opacity ' + this.tFade + 's linear');
-					var alpha = Math.random()*360 - 180;
+					//var alpha = Math.random()*360 - 180;
+					var alpha = charVal[i] ;
 					var beta = Math.random()*360;
-					var dist = Math.random()*this.radius;
-					$(p[i]).css('-webkit-transition-delay',this.tFall + 's')
-					$(p[i]).css('-webkit-transform', 'translate3d(' + dist*Math.cos(beta) + 'px,' + dist*Math.sin(beta) + 'px,0px) rotateZ(' + alpha + 'deg)');
+					var dist = this.radius*(0.75 + 0.25*Math.random());
+					var xSpread = w/2 -0.85*this.radius+alpha*55;
+					var yPos = -0.66*this.radius + this.letterCounts[alpha]*15;
+					$(p[i]).css('-webkit-transition-delay',this.tFall + 's');
+					$(p[i]).css('-webkit-transform', 'translate3d(' + xSpread + 'px,' + yPos + 'px,0px) rotateZ(360deg)' );
 					$(p[i]).css('opacity', '0');
 				};
 
