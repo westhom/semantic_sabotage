a67f7d1141da7af16baec97d74c56c78ec0f4a65
wadeaaron <wade@sosolimited.com>
2013-05-24 16:07:31 -0400

    oh-scope changes

--- fills/oh-scope.js
+++ fills/oh-scope.js
@@ -3,17 +3,25 @@ var mode = function(id) {
 	return {
 	
 		name: "Oh-Scope",
-		defaultURL: "http://www.youtube.com/watch?v=d1sRvT2xFL0",
+		defaultURL: "http://www.youtube.com/watch?v=WjuCI2yAVD8",
 		el: $('<div class="modeContainer" id="'+id+'"></div>'),	
 	
 		// Anything you want to do to initialize your mode. 
 		// This gets called once after the mode is created.
 		init: function() {
-			this.el.append('<div id="COS_container"><div id="COS_center"></div><div id="COS_marqueeContainer"></div></div>');
 		},
 
 		// Gets called evertime you go to the mode.
 		enter: function() {
+			this.el.empty();
+
+			var c = document.createElement('div');
+			$(c).addClass('container');
+			this.el.append(c);
+
+			$(c).append('<div class="marqueeCenter"></div>');
+//			$(c).append('<div class="particleCenter"></div>');
+
 		},
 
 		// Handle incoming word message.
@@ -37,36 +45,12 @@ var mode = function(id) {
 
 			var m = document.createElement('div');
 			$(m).append(msg.word);
-			$(m).addClass('COS_marquee proxima-nova-700');
-			$('#COS_marqueeContainer').append(m);
+			$(m).addClass('marquee proxima-nova-700');
+			$('#oh-scope .marqueeCenter').append(m);
 
-			/*
-			pronoun
-			article
-			verb
-			adverb
 
-			MAYBE SET 'quadrant' based on category
-			*/
 
-			var cats = ' ';
-			if ($.inArray('pronoun', msg.cats) >= 0) {
-				cats = cats + 'pronoun ';
-			}
-			if ($.inArray('article', msg.cats) >= 0) {
-				cats = cats + 'article ';
-			}
-			if ($.inArray('verb', msg.cats) >= 0) {
-				cats = cats + 'verb ';
-			}
-			if ($.inArray('adverb', msg.cats) >= 0) {
-				cats = cats + 'adverb ';
-			}
-
-			var k = 3;
-			var s = [];
-
-			var alpha = Math.random() * 180/k; /* <360 for gaps */
+/*
 			var dist = 50 + Math.random() * 400;
 
 			for (var i=0; i<k; i++) {
@@ -79,8 +63,6 @@ var mode = function(id) {
 				$('#COS_center').append(s[i]);
 			}
 			
-			window.getComputedStyle(s[k-1]).WebkitTransform;
-			
 			for (var i=0; i<k; i++) {
 				var beta = i*360/k + alpha;
 				$(s[i]).css('-webkit-transform', 'rotateZ(' + beta + 'deg) translateX(' + dist + 'px)');
@@ -88,8 +70,7 @@ var mode = function(id) {
 				$(m).css('z-index', '5');
 			}
 
-			$(m).css('opacity', '0.05');
-			$(m).css('color', 'black');
+*/			
 			
 		}
 	}
