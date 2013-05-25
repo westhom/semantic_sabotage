7e3d8ca1aad37bd59a47adcb2c6c62f5510be663
Wade Aaron <wade@sosolimited.com>
2013-05-25 13:29:10 -0400

    oh-scope rewrite

--- fills/oh-scope.js
+++ fills/oh-scope.js
@@ -5,6 +5,11 @@ var mode = function(id) {
 		name: "Oh-Scope",
 		defaultURL: "http://www.youtube.com/watch?v=WjuCI2yAVD8",
 		el: $('<div class="modeContainer" id="'+id+'"></div>'),	
+
+		storeID: 'empty',
+		storeClass: 'empty',
+		radius: 500,
+		period: 30,
 	
 		// Anything you want to do to initialize your mode. 
 		// This gets called once after the mode is created.
@@ -17,10 +22,14 @@ var mode = function(id) {
 
 			var c = document.createElement('div');
 			$(c).addClass('container');
+//			$(c).css('background-color','#666');
+			$(c).css('background-image','-webkit-gradient(radial, center center, ' + (this.radius) + ', center center, ' + (this.radius+10) + ', color-stop(0, #666), color-stop(1, #000))');
 			this.el.append(c);
 
+
 			$(c).append('<div class="marqueeCenter"></div>');
-//			$(c).append('<div class="particleCenter"></div>');
+
+			$('#oh-scope .marqueeCenter').css('width', this.radius+'px')
 
 		},
 
@@ -41,37 +50,54 @@ var mode = function(id) {
 		
 		appendWordInContext: function(msg) {
 		
-		 	// update curSentence
+			// update curSentence
 
-			var m = document.createElement('div');
-			$(m).append(msg.word);
-			$(m).addClass('marquee proxima-nova-700');
-			$('#oh-scope .marqueeCenter').append(m);
+//			console.log('word: '+msg.word);
 
-
-
-/*
-			var dist = 50 + Math.random() * 400;
-
-			for (var i=0; i<k; i++) {
-				s[i] = document.createElement('div');
-				$(s[i]).append(msg.word);
-				$(s[i]).addClass('COS_particle proxima-nova-700');
-				$(s[i]).addClass(cats);
-				var beta = i*360/k + alpha;
-				$(s[i]).css('-webkit-transform', 'rotateZ(' + beta + 'deg) translateX(0px)');
-				$('#COS_center').append(s[i]);
-			}
-			
-			for (var i=0; i<k; i++) {
-				var beta = i*360/k + alpha;
-				$(s[i]).css('-webkit-transform', 'rotateZ(' + beta + 'deg) translateX(' + dist + 'px)');
-				$(s[i]).css('opacity', '0.25');
-				$(m).css('z-index', '5');
+			if (this.storeID[0] == '[') {
+				this.storeID = this.storeID + msg.word;
 			}
 
-*/			
-			
+			if (msg.word == ']:' ) {
+				if (this.storeID.search('Tower') >= 0) this.storeClass = 'tower';
+				if (this.storeID.search('AirFrance') >= 0) this.storeClass = 'airfrance';
+				if (this.storeID.search('Comair') >= 0) this.storeClass = 'comair';
+				console.log('storeID: ' + this.storeID + ' -- storeClass: ' + this.storeClass);
+				this.storeID = 'empty';
+			}
+
+			if (this.storeID.search('unintell') >=0) {
+				msg.word = '[unintelligible]';
+				this.storeID = 'empty';
+			}
+
+			if (this.storeID == 'empty') {
+				if (msg.word == '[') {
+					this.storeID = msg.word;
+				}
+				else if ($.inArray('punct', msg.cats) < 0) {
+					var m = document.createElement('div');
+					$(m).append(msg.word);
+					$(m).addClass('marquee proxima-nova-700');
+					$('#oh-scope .marqueeCenter').append(m);
+
+					setTimeout( function() { $(m).remove() }, this.period*1000 );
+
+//					var r = 10;
+//					var r = 10 + this.radius-75-10;
+					var r = 10 + Math.round(Math.random()*(this.radius-75-10));
+					var p = document.createElement('div');
+					console.log(msg.word);
+					console.log(msg.cats);
+					console.log(msg.stat)
+					$(p).append(msg.word);
+					$(p).addClass('particle ' + this.storeClass + ' proxima-nova-400');
+					$(p).css('left', r +'px');
+					$(p).css('-webkit-transform-origin', -r/10 + '% 50%');
+					$('#oh-scope .marqueeCenter').append(p);
+				}
+				
+			}			
 		}
 	}
 };
