71d3a995a6fa8df20a77a16760259c3ce0755681
Seth Thompson <s3th.thompson@gmail.com>
2013-06-04 15:44:28 -0400

    bringing youtube-local up to date, closes #64

--- php/youtube_load.php
+++ php/youtube_load.php
@@ -4,6 +4,9 @@
 	parse_str( parse_url( $_POST["url"], PHP_URL_QUERY ), $url_fragments);
 
 	$response = file_get_contents("./responses/".$url_fragments['v'].".json");
+
+	sleep(3);
+
 	echo $response;
 	
 		
